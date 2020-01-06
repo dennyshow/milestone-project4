4ed3f8f54219d04123c86fb372058f7f56bbf7a1
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-06 18:31:39 +0000

    changed models and fixing error with dbs

--- home/models.py
+++ home/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from bids.models import Bid
+from products.models import Product
 
 # Create your models here.
 
 class Page(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    bid_id = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     image = models.ImageField(upload_to="images")
     
     def __str__(self):
-        return "user_id:" + str(self.user_id) + "bid_id:" + str(self.bid_id)
+        return "user_id:" + str(self.user_id) + "product_id:" + str(self.product_id)
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import home_view, bid_from_home
+from .views import home_view
 
 urlpatterns = [
     url(r'^$', home_view, name="home"),
-    url(r'^home_bid/(?P<auction_id>)', bid_from_home, name="bid_from_home")
+    
     
     ]
