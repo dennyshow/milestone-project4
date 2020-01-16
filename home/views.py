695be8e9223ea54ff2b562362cd3b8d2d34c546c
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-16 13:18:30 +0000

    allowing user bid to save from homepage

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ from auctions.models import Auction
 from bids.models import Bid
 from django.db.models import Q
 
-# Create your views here.
+
 
 def home_view(request):
     home = Page.objects.filter()
@@ -26,23 +26,7 @@ def bid_from_home(request):
                     product = Product.objects.get(id=p_id)
                     product.auction_price += int(request.POST['bid'])
                     product.save()
-                    
-                    bids = Bid.objects.filter(product_id=p_id)
-                    if bids:
-                        bid = bids[0]
-                        bid.user_id = request.user
-                        bid.bid_time = timezone.now()
-                        bid.bid_views += 1
-                        bid.save()
-                    
-                    else:    
-                        new_bid = Bid()
-                        new_bid.product_id = product
-                        new_bid.auction_id = auction
-                        new_bid.user_id = request.user
-                        new_bid.bid_time = timezone.now()
-                        new_bid.bid_views += 1
-                        new_bid.save()
+        
                     messages.error(request, "Bidding is done!")
                 else:
                     messages.error(request, "Bidding is closed!")
