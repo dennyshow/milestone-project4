7f76b2d69e4602aba18e3bcb52788bb0aeb1ec94
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-16 10:12:56 +0000

    Allow winner to only make payment

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, reverse, redirect
 from django.utils import timezone
 import stripe # new
+from auctions.models import Auction
+from bids.models import Bid
+
+from django.contrib import messages
+
 
 from django.conf import settings
 from django.views.generic.base import TemplateView
@@ -20,20 +25,31 @@ class view_basket(TemplateView):
         return context
         
         
-            # To Charge Page
+# To Charge Page
 def charge(request): # new
     if request.method == 'POST':
-        if 'bid_price' in request.POST:
-            bid_price = request.POST['bid_price']
+        product_id = request.POST['bid_product']
+        print(product_id)
+        auction = Auction.objects.get(product_id=product_id)
+        bid = Bid.objects.get(product_id=product_id)
+        if timezone.now() >= auction.end_time:
+            print(request.user.id, bid.user_id.id)
+            if request.user.id == bid.user_id.id:
+                if 'bid_price' in request.POST:
+                    bid_price = request.POST['bid_price']
+                else:
+                    bid_price = 500
+                charge = stripe.Charge.create(
+                    amount=500,
+                    currency='usd',
+                    description='A Django charge',
+                    source=request.POST['stripeToken']
+                )
+                return render(request, 'charge.html', {'price':bid_price})
+            else:
+                messages.error(request, "Only winner can purchase!")
         else:
-            bid_price = 500
-        charge = stripe.Charge.create(
-            amount=500,
-            currency='usd',
-            description='A Django charge',
-            source=request.POST['stripeToken']
-        )
-        return render(request, 'charge.html', {'price':bid_price})
-     
+            messages.error(request, "Bidding is going on!")
+    return redirect(reverse('bid'))
     
     
