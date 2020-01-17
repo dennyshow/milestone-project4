0fdc9d2f018987ac35dcb3bcc06ac8da7eec3d62
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-17 04:48:56 +0000

    commiting untracked files

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_WUPAiYK8aeAFNZzS29U6rs92001W2TiuKa")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_qMBJFx9yyJHsCbSNfG33NlH000SH989oy6")
+os.environ.setdefault("SECRET_KEY", "%5eb(f$&wufnauo%1f+rw4uw)p$!1ystqpay)vhz^8fp7zw)%a")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAIMZ24JIBAA4MDLXQ")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
