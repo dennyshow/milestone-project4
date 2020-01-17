14021560b2ce729c50ddadefd1d0a2e21ecde2b1
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-17 01:14:49 +0000

    added more models for user profiles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Bidder(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=30)
+    street_address1 = models.CharField(max_length=100)
+    street_address2 = models.CharField(max_length=100)
+    county = models.CharField(max_length=20)
+    eircode = models.CharField(max_length=10)
+    country = models.CharField(max_length=20)
+    
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Bidder.objects.create(user=instance)
+    instance.bidder.save()
