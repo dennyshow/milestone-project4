e27c52acc1af5bc02ec19e71be5873627c0765c0
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-18 00:15:33 +0000

    added some comment

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Bidder(models.Model):
-    user = models.OneToOneField(User)
+        # creating model to store bidder/user info 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=30)
     street_address1 = models.CharField(max_length=100)
     street_address2 = models.CharField(max_length=100)
@@ -12,7 +14,9 @@ class Bidder(models.Model):
     eircode = models.CharField(max_length=10)
     country = models.CharField(max_length=20)
     
+    
 @receiver(post_save, sender=User)
+        # updating into profile when a new user/bidder has been created
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Bidder.objects.create(user=instance)
