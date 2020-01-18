1db2d2fdb468b0bb3e6aed4499e9a83c84cfd369
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-18 00:09:04 +0000

    added inasseccabilty for unathorized user and added more comments

--- bids/views.py
+++ bids/views.py
@@ -10,15 +10,21 @@ from django.conf import settings
 
 
 def all_bids(request):
-    
-         # display all current and expired bids
-    bid = Bid.objects.all()
-    endtime_list = []
+         # display all ongoing and expired bids
+         # Let bid available to only registered user/bidder
+         # Else returns the unathorized user back to home
+    if request.user.is_authenticated:
+        bid = Bid.objects.all()
+        endtime_list = []
         # the end of auction to allow user make payment
-    for a in bid:
-        endtime_list.append(str(a.auction_id.end_time))
-    publishable_key = settings.STRIPE_PUBLISHABLE
-    return render(request, "bid.html", {"bid": bid, 'key':publishable_key, 'end_time':endtime_list})
+        for a in bid:
+            endtime_list.append(str(a.auction_id.end_time))
+        publishable_key = settings.STRIPE_PUBLISHABLE
+        return render(request, "bid.html", {"bid": bid, 'key':publishable_key, 'end_time':endtime_list})
+    else:
+        messages.error(request, "You must be a registered user to view a bid!")
+        return redirect(reverse('home'))
+    return render(request, "home.html")
 
 
 
