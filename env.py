0c626d27fe5b8cc117152022c839d5ce26fdfe25
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-21 07:13:22 +0000

    removing database_url for travis build error

--- env.py
+++ env.py
@@ -1,6 +1,5 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", "postgres://vfwliomcdxbwcc:a268bf3e06a27ee44edd816fbe770ac041174e322143e5daed0603e75cc9c4c6@ec2-54-217-219-235.eu-west-1.compute.amazonaws.com:5432/dfj4bo3aq3470r")
 os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_WUPAiYK8aeAFNZzS29U6rs92001W2TiuKa")
 os.environ.setdefault("STRIPE_SECRET", "sk_test_qMBJFx9yyJHsCbSNfG33NlH000SH989oy6")
 os.environ.setdefault("SECRET_KEY", "%5eb(f$&wufnauo%1f+rw4uw)p$!1ystqpay)vhz^8fp7zw)%a")
