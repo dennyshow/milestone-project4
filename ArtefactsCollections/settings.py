1c8fbecf877af5ad2ec0ece247a13d48c5f681a2
EC2 Default User <ec2-user@ip-172-31-47-222.ec2.internal>
2020-01-21 09:06:16 +0000

    fixed issue with secret key

--- ArtefactsCollections/settings.py
+++ ArtefactsCollections/settings.py
@@ -148,7 +148,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_STORAGE_BUCKET_NAME = 'denny-auctionsite'	
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")	
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")	
-
+print(AWS_SECRET_ACCESS_KEY)
 
 AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'	
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME	
